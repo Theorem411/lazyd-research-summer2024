ab5148ac5b5dadb41be4c58d17e0943e9fdf3f5b
Henry Nelson <hcnelson99@gmail.com>
2019-07-24 13:53:59 -0400

    cache-aware build script

---
+++ llvm/build.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+mkdir -p build
+cd build
+if [ ! -f build.ninja ]; then
+    cmake -G Ninja .. \
+    -DLLVM_USE_SPLIT_DWARF=TRUE \
+    -DLLVM_ENABLE_ASSERTIONS=TRUE \
+    -DCMAKE_BUILD_TYPE=Debug \
+    -DCMAKE_INSTALL_PREFIX="$HOME/opt/clang-uli" \
+    -DCMAKE_SHARED_LINKER_FLAGS="-Wl,--gdb-index,-fuse-ld=gold" \
+    -DCMAKE_EXE_LINKER_FLAGS="-Wl,--gdb-index,-fuse-ld=gold" \
+    -DBUILD_SHARED_LIBS=true \
+    -DLLVM_TARGETS_TO_BUILD=X86 \
+    2>&1 | tee cmake.log
+fi
+ninja
