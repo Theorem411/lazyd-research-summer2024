54048bf84cef587ef82630d841085bc5b457568c
Henry Nelson <hcnelson99@gmail.com>
2019-07-30 14:57:04 -0400

    caching with ccache

--- llvm/build.sh
+++ llvm/build.sh
@@ -9,8 +9,10 @@ if [ ! -f build.ninja ]; then
     -DCMAKE_INSTALL_PREFIX="$HOME/opt/clang-uli" \
     -DCMAKE_SHARED_LINKER_FLAGS="-Wl,--gdb-index,-fuse-ld=gold" \
     -DCMAKE_EXE_LINKER_FLAGS="-Wl,--gdb-index,-fuse-ld=gold" \
+    -DCMAKE_EXE_LINKER_FLAGS="-Wl,--gdb-index,-fuse-ld=gold" \
     -DBUILD_SHARED_LIBS=true \
     -DLLVM_TARGETS_TO_BUILD=X86 \
+    -DLLVM_CCACHE_BUILD=true \
     2>&1 | tee cmake.log
 fi
 ninja -j2
