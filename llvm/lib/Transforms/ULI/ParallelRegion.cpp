b5001995eda8eef019ef9778e09cff5f4b5701a0
Ziqi Liu <ziqil2@andrew.cmu.edu>
2024-02-08 02:42:10 -0500

    first debuggable version

--- llvm/lib/Transforms/ULI/ParallelRegion.cpp
+++ llvm/lib/Transforms/ULI/ParallelRegion.cpp
@@ -19,7 +19,7 @@
 #include "llvm/IR/DebugLoc.h"
 #include "llvm/Support/raw_ostream.h"
 
-#define PR_NAME "parallel-region"
+#define PR_NAME "parallel-region-reachable"
 #define DEBUG_TYPE PR_NAME
 
 using namespace llvm;
@@ -39,8 +39,124 @@ STATISTIC(
     "Number of functions untouched by the end of this call-graph analysis,"
     " usually root function in the callgraph.");
 
-class ParallelRegionImpl {
-public:
+// DEBUG: Note on how to check the function attribute defined in LoopSpawningTI
+//   Function *F = CGN->getFunction();
+//   //   assert(F && "encounter callgraph node with null function");
+//   // TODO: for each callnode in the callgrpah, identify fn attr using
+//   return F && F->getFnAttribute("parallel-region").getValueAsString() ==
+//   "true";
+
+// PreservedAnalyses ParallelRegionPass::run(Module &M,
+//                                           ModuleAnalysisManager &AM) {
+// // auto &FAM = AM.getResult<FunctionAnalysisManagerModuleProxy>(M).getManager();
+// //   DominatorTree &DT = AM.getResult<DominatorTreeAnalysis>(M);
+//   CallGraph &CG = AM.getResult<CallGraphAnalysis>(M);
+//   bool Changed = ParallelRegionImpl(M, CG, TI).run();
+//   if (Changed)
+//     return PreservedAnalyses::none();
+//   return PreservedAnalyses::all();
+// }
+
+namespace {
+struct ParallelRegionReachable : public ModulePass {
+  static char ID;
+
+  explicit ParallelRegionReachable() : ModulePass(ID) {
+    // initializeParallelRegionReachablePass(*PassRegistry::getPassRegistry());
+  }
+
+  bool runOnModule(llvm::Module &M) override {
+    CallGraph &CG = getAnalysis<CallGraphWrapperPass>().getCallGraph();
+    outs() << "callgraph analysis succceed!\n";
+    // initialize GlobalFnStates to all Untouched
+    initializeGlobalFnStates(CG);
+
+    // initialize worklist
+    SmallVector<CallGraphNode *, 8> workList;
+    initializeWorkList(CG, workList);
+
+    // while worklist non-empty,
+    // populate
+    for (size_t i = 0; i < workList.size(); ++i) {
+        CallGraphNode *CGN = workList[i];
+        assert(CGN && "worklist shouldn't have null callnode!");
+
+        Function *Caller = CGN->getFunction();
+        assert(Caller && "CallNode has null function!");
+        if (Caller->isDeclaration()) {
+            continue;
+        }
+
+        outs() << "pop CallNode " << Caller->getName() << "...\n";
+
+        // get TapirTaskInfo for the caller function
+        TaskInfo &TI = getAnalysis<TaskInfoWrapperPass>(*Caller).getTaskInfo();
+
+        // for each callgraph node, traverse through each of its callsite
+        for (auto &CallRecord : *CGN) {
+            // in the case of callback function, there is no callsite
+            if (!CallRecord.first.hasValue()) {
+                // TODO: this function has its address taken? 
+                continue;
+            }
+            // if call node has explicit callsite
+            assert(CallRecord.second && "encounter null second field in CallRecord!");
+            Function *Callee = CallRecord.second->getFunction();
+            assert(Callee && "CallRecord contains null callee node!");
+            const CallBase *CallSite = dyn_cast<CallBase>(*CallRecord.first);
+            assert(CallSite &&
+                    "CallRecord doesn't have CallBase callsite instruction!");
+
+            LLVM_DEBUG(dbgs() << "  examing callsite at ln: << " << getLine(CallSite));
+            CallSite->dump();
+            LLVM_DEBUG(dbgs() << "\n");
+            
+            // check if callsite is in a parallel-region
+            const Task *T = TI[CallSite->getParent()];
+            if (!T) {
+                if (!Caller) {
+                    errs() << "Callsite "
+                            << "null"
+                            << "->" << Callee->getName() << " has null task!\n";
+                } else {
+                    errs() << "Callsite " << Caller->getName() << "->"
+                            << Callee->getName() << " has null task!\n";
+                }
+            }
+            assert(T && "Callsite contains null task. There should be a root task!");
+
+            // update state of callee node based on caller & parallel-region info
+            FnState sOld = getState(Callee);
+            if (T->isSerial()) {
+                // callsite is in a serial region
+                updateState(Callee, getState(Caller));
+                updateState(Callee, FnState::DefinitelyEF);
+            } else {
+                // callsite is in a parallel region
+                updateState(Callee, FnState::DefinitelyDAC);
+            }
+            FnState sNew = getState(Callee);
+
+            // push Callee back on to worklist because of state change
+            if (sOld != sNew) {
+                workList.push_back(CallRecord.second);
+            }
+        }
+    }
+
+    // set statistics
+    printStatistic();
+    return false;
+    
+  }
+
+  void getAnalysisUsage(AnalysisUsage &AU) const override {
+    // AU.addRequired<DominatorTreeWrapperPassF>();
+    AU.addRequired<CallGraphWrapperPass>();
+    AU.addRequired<TaskInfoWrapperPass>();
+    AU.setPreservesAll();
+  }
+private: 
   enum class FnState {
     // trivial state for initialization and for root functions
     Untouched,
@@ -53,42 +169,34 @@ public:
     // for function whose address has been taken
     // External
   };
-
-  ParallelRegionImpl(Module &M, CallGraph &CG, DominatorTree &DT, TaskInfo &TI)
-      : M(M), CG(CG), DT(DT), TI(TI){};
-  bool run();
-
-private:
-  //   bool isParallelRegion(CallGraphNode *CGN); // no longer needed
-  FnState joinState(Fnstate s1, FnState s2) {
-    switch (s1) {
-    case FnState::Both: {
+private: 
+  FnState joinState(FnState s1, FnState s2) {
+    switch(s1) {
+    case ParallelRegionReachable::FnState::Both: {
       return s1;
     }
-    case FnState::Untouched: {
+    case ParallelRegionReachable::FnState::Untouched: {
       return s2;
     }
-    case FnState::DefinitelyDAC: {
-      switch
-        s2 {
-        case FnState::DefinitelyDAC: {
+    case ParallelRegionReachable::FnState::DefinitelyDAC: {
+      switch(s2) {
+        case ParallelRegionReachable::FnState::DefinitelyDAC: {
           return s2;
         }
         default: {
-          return FnState::Both;
+          return ParallelRegionReachable::FnState::Both;
         }
-        }
+      }
     }
-    case FnState::DefinitelyEF: {
-      switch
-        s2 {
-        case FnState::DefinitelyEF: {
+    case ParallelRegionReachable::FnState::DefinitelyEF: {
+      switch(s2) {
+        case ParallelRegionReachable::FnState::DefinitelyEF: {
           return s2;
         }
         default: {
-          return FnState::Both;
+          return ParallelRegionReachable::FnState::Both;
         }
-        }
+      }
     }
     }
   }
@@ -106,14 +214,14 @@ private:
     GlobalFnStates[F] = joinState(stateOld, stateNew);
   }
 
-  void initializeGlobalFnStates() {
-    LLVM_DEBUG(dbgs() << "calling initializeGlobalFnStates...\n");
+  void initializeGlobalFnStates(CallGraph& CG) {
+    outs() << "calling initializeGlobalFnStates...\n";
 
     for (auto &it : CG) {
       const Function *F = it.first;
       if (!F)
         continue;
-      GlobalFnStates.insert(F, FnState::Untouched);
+      GlobalFnStates[F] = ParallelRegionReachable::FnState::Untouched;
       // update global satistic: NumFn
       ++NumFn;
 
@@ -123,7 +231,7 @@ private:
     LLVM_DEBUG(dbgs() << "\n\n");
   }
 
-  void initializeWorkList(SmallVector<CallGraphNode *, 8> &workList) {
+  void initializeWorkList(CallGraph& CG, SmallVector<CallGraphNode *, 8>& workList) {
     LLVM_DEBUG(dbgs() << "calling initializeWorkList...\n");
     for (auto &it : CG) {
       const Function *F = it.first;
@@ -139,259 +247,73 @@ private:
     LLVM_DEBUG(dbgs() << "\n\n");
   }
 
-  void printStatistic();
-
-  // @debug
-  unsigned getLine(CallBase *cs) {
-    return cs->getDebugLoc().getLine();
-  } 
-
-private:
-  Module &M;
-  CallGraph &CG;
-  DominatorTree &DT;
-  TaskInfo &TI;
-
-  // record all callnodes that corresponds to definitely dac
-  //   SmallSet<CallGraphNode *, 8> definitelyDACOutlineFn;
-  //   SmallSet<CallGraphNode *, 8> parallelRegionOutlineFn;
-  DenseMap<Function *, FnState> GlobalFnStates;
-};
-
-// bool ParallelRegionImpl::isParallelRegion(CallGraphNode *CGN) {
-//   Function *F = CGN->getFunction();
-//   //   assert(F && "encounter callgraph node with null function");
-//   // TODO: for each callnode in the callgrpah, identify fn attr using
-//   return F && F->getFnAttribute("parallel-region").getValueAsString() ==
-//   "true";
-// }
-void ParallelRegionImpl::printStatistic() {
-  for (auto it : GlobalFnStates) {
-    switch
-      it.second {
-      case FnState::DefinitelyDAC: {
-        ++NumDefinitelyDAC;
-      }
-      case FnState::DefinitelyEF: {
-        ++NumDefinitelyEF;
-      }
-      case FnState::Both: {
-        ++NumBoth;
-      }
-      case FnState::Untouched: {
-        ++NumUntouched;
-      }
-      }
-  }
-}
-
-bool ParallelRegionImpl::run() {
-  // initialize GlobalFnStates to all Untouched
-  initializeGlobalFnStates();
-
-  // initialize worklist
-  SmallVector<CallGraphNode *, 8> workList;
-  initializeWorkList(workList);
-
-  // while worklist non-empty,
-  //   populate
-  for (size_t i = 0; i < workList.size(); ++i) {
-    CallGraphNode *CGN = workList[i];
-    assert(CGN && "worklist shouldn't have null callnode!");
-
-    Function *Caller = CGN->getFunction();
-
-    LLVM_DEBUG(dbgs() << "pop CallNode " << Caller->getname() << "...\n");
-
-    // for each callgraph node, traverse through each of its callsite
-    for (auto &CallRecord : *CGN) {
-      // in the case of callback function, there is no callsite
-      if (!CallRecord.first.hasValue()) {
-        continue;
-      }
-      // if call node has explicit callsite
-      assert(CallRecord.second && "encounter null second field in CallRecord!");
-      Function *Callee = CallRecord.second->getFunction();
-      assert(Callee && "CallRecord contains null callee node!");
-      CallBase *CallSite = dyn_cast<CallBase>(CallRecord.first->get());
-      assert(CallSite &&
-             "CallRecord doesn't have CallBase callsite instruction!");
-
-      LLVM_DEBUG(dbgs() << "  examing callsite at ln: << " << getLine(CallSite));
-      CallSite->dump();
-      LLVM_DEBUG(dbgs() << "\n");
-      
-      // check if callsite is in a parallel-region
-      Task *T = TI[CallSite->getParent()];
-      if (!T) {
-        if (!Caller) {
-          errs() << "Callsite "
-                 << "null"
-                 << "->" << Callee->getname() << " has null task!\n";
-        } else {
-          errs() << "Callsite " << Caller->getName() << "->"
-                 << Callee->getname() << " has null task!\n";
+  void printStatistic() {
+    for (auto it : GlobalFnStates) {
+        switch(it.second) {
+        case ParallelRegionReachable::FnState::DefinitelyDAC: {
+            ++NumDefinitelyDAC;
+            break;
         }
-      }
-      assert(T && "Callsite contains null task. There should be a root task!");
-
-      // update state of callee node based on caller & parallel-region info
-      FnState sOld = getState(Callee);
-      if (T->isSerial()) {
-        // callsite is in a serial region
-        updateState(Callee, getState(Caller));
-        updateState(Callee, FnState::DefinitelyEF);
-      } else {
-        // callsite is in a parallel region
-        updateState(Callee, FnState::DefinitelyDAC);
-      }
-      FnState sNew = getState(Callee);
-
-      // push Callee back on to worklist because of state change
-      if (sOld != sNew) {
-        workList.push_back(CallRecord.second);
-      }
+        case ParallelRegionReachable::FnState::DefinitelyEF: {
+            ++NumDefinitelyEF;
+            break;
+        }
+        case ParallelRegionReachable::FnState::Both: {
+            ++NumBoth;
+            break;
+        }
+        case ParallelRegionReachable::FnState::Untouched: {
+            ++NumUntouched;
+            break;
+        }
+        }
     }
   }
 
-  //   // worklist algorithm
-  //   SmallVector<CallGraphNode *, 8> workList;
-  //   SmallSet<CallGraphNode *, 8> workSet;
+  // @debug
+  unsigned getLine(const CallBase *cs) {
+    return cs->getDebugLoc().getLine();
+  } 
 
-  //   // initialize worklist with callee nodes of Parallel-region nodes
-  //   for (auto &it : CG) {
-  //     CallGraphNode *CGNode = it.second.get();
-  //     assert(CGNode && "encounter null call graph node");
-
-  //     // update total function counting
-  //     outs() << "Function #" << NumFn;
-  //     if (!CGNode->getFunction()) {
-  //       outs() << ": null\n";
-  //     } else {
-  //       Function *Fn = CGNode->getFunction();
-  //       outs() << ": " << Fn->getName() << "\n"; //  <<
-  //                                                //
-  //                                                Fn->getFnAttribute("parallel-region").getKindAsString()
-  //                                                //  << "\n";
-  //       ++NumFn;
-  //     }
-
-  //     if (isParallelRegion(CGNode)) {
-  //       // increment parallel-region count
-  //       parallelRegionOutlineFn.insert(CGNode);
-  //       ++NumParallelRegions;
-  //       // populate worklist with first layer of callee nodes of
-  //       parallel-regions for (auto &CallRecord : *CGNode) {
-  //         if (!CallRecord.first.hasValue()) {
-  //           // in the case of callback function, there is no callsite
-  //           continue;
-  //         }
-  //         CallGraphNode *CalleeNode = CallRecord.second;
-  //         assert(CalleeNode && "encounter null second field in CallRecord!");
-
-  //         Function *Callee = CalleeNode->getFunction();
-  //         if (!Callee) //
-  //           continue;
-  //         workList.push_back(CGNode);
-  //       }
-  //     }
-  //   }
-
-  //   // worklist algorithm through call graph, mark each callgraph node as DAC
-  //   if outs() << "begin propagating worklist...\n";
-  //   //   outs() << "worklist has size = " << workList.size() << "\n";
-  //   for (size_t i = 0; i < workList.size(); ++i) {
-
-  //     outs() << i << ": " << workList.size() << "\n";
-  //     CallGraphNode *CGN = workList[i];
-  //     assert(CGN && "worklist shouldn't have null callnode!");
-
-  //     // put callee nodes of current back on worklist
-  //     for (auto &CallRecord : *CGN) {
-  //       if (!CallRecord.first.hasValue()) {
-  //         // in the case of callback function, there is no callsite
-  //         continue;
-  //       }
-  //       CallGraphNode *CalleeNode = CallRecord.second;
-  //       assert(CalleeNode && "encounter null second field in CallRecord!");
-
-  //       Function *Callee = CalleeNode->getFunction();
-  //       if (!Callee)
-  //         continue;
-
-  //       if (workSet.find(CalleeNode) == workSet.end()) {
-  //         workList.push_back(CalleeNode);
-  //       }
-  //     }
-  //   }
-
-  // DEBUG: final printing
-  //   outs() << "printing parallel region outline\n";
-  //   for (CallGraphNode *CGN : parallelRegionOutlineFn) {
-  //     Function *F = CGN->getFunction();
-  //     assert(F && "parallelRegionOutlineFn shouldn't have null function");
-  //     outs() << F->getName() << "\n";
-  //   }
-  //   outs() << "\n";
-
-  //   outs() << "printing definitelyDACOutlineFn\n";
-  //   for (CallGraphNode *CGN : definitelyDACOutlineFn) {
-  //     Function *F = CGN->getFunction();
-  //     assert(F && "definitelyDACOutlinFn shouldn't have null function");
-  //     outs() << F->getName() << "\n";
-  //   }
-
-  // set statistics
-  printStatistic();
-  return false;
-}
-
-PreservedAnalyses ParallelRegionPass::run(Module &M,
-                                          ModuleAnalysisManager &AM) {
-  DominatorTree &DT = AM.getResult<DominatorTreeAnalysis>(F);
-  CallGraph &CG = AM.getResult<CallGraphAnalysis>(M);
-  TaskInfo &TI = AM.getResult<TaskAnalysis>(F);
-  bool Changed = ParallelRegionImpl(M, CG, DT, TI).run();
-  if (Changed)
-    return PreservedAnalyses::none();
-  return PreservedAnalyses::all();
-}
-
-namespace {
-struct ParallelRegion : public ModulePass {
-  static char ID;
-
-  explicit ParallelRegion() : ModulePass(ID) {
-    // llvm::initializeParallelRegionPass(*PassRegistry::getPassRegistry());
-  }
-
-  bool runOnModule(Module &M) override {
-    DominatorTree &DT = getAnalysis<DominatorTreeWrapperPass>().getDomTree();
-    TaskInfo &TI = getAnalysis<TaskInfoWrapperPass>().getTaskInfo();
-    CallGraph &CG = getAnalysis<CallGraphWrapperPass>().getCallGraph();
-
-    return ParallelRegionImpl(M, CG, DT, TI).run();
-  }
-
-  void getAnalysisUsage(AnalysisUsage &AU) const override {
-    AU.addRequired<CallGraphWrapperPass>();
-    AU.setPreservesAll();
-  }
+private: 
+  DenseMap<const Function *, FnState> GlobalFnStates;
 };
 } // namespace
 
-char ParallelRegion::ID = 0;
-static RegisterPass<ParallelRegion> X("parallel-region",
+
+
+
+char ParallelRegionReachable::ID = 0;
+static RegisterPass<ParallelRegionReachable> X(PR_NAME,
                                       "Conduct CallGraph Analysis to determine "
-                                      "outlining fashion of parallel-regions");
+                                      "outlining fashion of parallel-regions",
+                                      false, true);
 
-// INITIALIZE_PASS(ParallelRegion, PR_NAME, pr_name, false, false)
-// static const char pr_name[] = "Conduct CallGraph Analysis to determine "
-//                               "outlining fashion of parallel-regions";
-// INITIALIZE_PASS_BEGIN(ParallelRegion, "ParallelRegion", pr_name, false,
-// false) INITIALIZE_PASS_DEPENDENCY(CallGraphWrapperPass)
-// INITIALIZE_PASS_END(ParallelRegion, "ParallelRegion", pr_name, false, false)
+static const char pr_name[] = "Conduct CallGraph Analysis to determine "
+                              "outlining fashion of functions called inside parallel-regions";
+// static void* initializeParallelRegionPassOnce(PassRegistry &Registry) {
+//     initializeTaskInfoWrapperPassPass(Registry);
+//     initializeCallGraphWrapperPassPass(Registry);
+//     PassInfo *PI = new PassInfo(                                             
+//                     pr_name, PR_NAME, &ParallelRegionReachable::ID,
+//                     PassInfo::NormalCtor_t(callDefaultCtor<ParallelRegion>), false, false);
+//     Registry.registerPass(*PI, true); 
+//     return PI;
+// }
+// static llvm::once_flag InitializeParallelRegionPassFlag; 
+// void initializeParallelRegionPass(PassRegistry &Registry) {
+//     llvm::call_once(InitializeParallelRegionPassFlag,
+//                     initializeParallelRegionPassOnce, std::ref(Registry));
+// }
 
-// namespace llvm {
-// Pass *createParallelRegionPass() { return new ParallelRegion(); }
-// } // namespace llvm
+///DEBUG: change ParallelRegion to a different name; there's likely a name conflict in llvm
+
+// INITIALIZE_PASS(ParallelRegionReachable, PR_NAME, pr_name, false, false)
+// INITIALIZE_PASS_BEGIN(ParallelRegionReachable, PR_NAME, pr_name, false, false) 
+// INITIALIZE_PASS_DEPENDENCY(CallGraphWrapperPass)
+// INITIALIZE_PASS_DEPENDENCY(TaskInfoWrapperPass)
+// INITIALIZE_PASS_END(ParallelRegionReachable, PR_NAME, pr_name, false, false)
+
+namespace llvm {
+Pass *createParallelRegionPass() { return new ParallelRegionReachable(); }
+}
